16bc10bc6d55e8dc71a0dd027368d5abcd89855e
lrandria <ranlauren633@gmail.com>
2023-05-04 11:12:29 +0200

    monster6 et monster5

--- core/src/spacecraft/Monster5.java
+++ core/src/spacecraft/Monster5.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class Monster5 extends Alien{
 
-    private static String name = "Monter_5";
-    private static Texture picture = new Texture("");
-
+    private static String DEFAULT_NAME = "Monster5";
+    private static int DEFAULT_MAX_LIFE = 100;
+    private static int DEFAULT_LIFE = 100;
+    private static Texture DEFAULT_PICTURE = new Texture(" ");
     public Monster5(){
-        super(name,picture);
-        setLife(5);
-        setMaxLife(5);
+        super(DEFAULT_NAME,DEFAULT_PICTURE);
+        setLife(DEFAULT_LIFE);
+        setMaxLife(DEFAULT_MAX_LIFE);
     }
 }
--- core/src/spacecraft/Monster6.java
+++ core/src/spacecraft/Monster6.java
@@ -4,12 +4,13 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class Monster6 extends Alien{
 
-    private static String name = "Monter_6";
-    private static Texture picture = new Texture("");
-
+    private static String DEFAULT_NAME = "Monster6";
+    private static int DEFAULT_MAX_LIFE = 60;
+    private static int DEFAULT_LIFE = 60;
+    private static Texture DEFAULT_PICTURE = new Texture(" ");
     public Monster6(){
-        super(name,picture);
-        setLife(60);
-        setMaxLife(60);
+        super(DEFAULT_NAME,DEFAULT_PICTURE);
+        setLife(DEFAULT_LIFE);
+        setMaxLife(DEFAULT_MAX_LIFE);
     }
 }
