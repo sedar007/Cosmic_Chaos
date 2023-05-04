fd738dc62795539ef7b980e2e1d1b8d36a39edab
lrandria <ranlauren633@gmail.com>
2023-05-04 10:06:46 +0200

    init

--- core/src/spacecraft/Spacecraft.java
+++ core/src/spacecraft/Spacecraft.java
@@ -10,13 +10,27 @@ public class Spacecraft {
     protected int stamina;
     protected int maxStamina;
 
-    public Spacecraft(String name){
-        this.name = name;
-        this.life = 100;
-        this.maxLife = 100;
-    }
-    public Spacecraft(){
+    public String getName(){ return this.name;}
+    public void setName(String name){ this.name = name;}
 
+    public int getLife() { return this.life;}
+    public void setLife(int life){ this.life = life;}
+
+    public int getStamina(){ return this.stamina;}
+    public void setStamina(int stamina){ this.stamina = stamina;}
+
+    public int getMaxLife(){ return this.maxLife;}
+    public void setMaxLife(int maxLife) {this.maxLife = maxLife;}
+
+    public int getMaxStamina(){ return this.maxStamina;}
+    public void setMaxStamina(int maxStamina) {this.maxStamina = maxStamina;}
+
+    public Spacecraft(String name){
+        setName(name);
+    }
+
+    public Spacecraft(){
+        this("Vessel");
     }
 
 
