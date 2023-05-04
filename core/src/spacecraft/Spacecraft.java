674043d01e9e2fcf22272a68a911dd29ae294605
lrandria <ranlauren633@gmail.com>
2023-05-04 09:53:02 +0200

    init

--- core/src/spacecraft/Spacecraft.java
+++ core/src/spacecraft/Spacecraft.java
@@ -1,4 +1,25 @@
 package spacecraft;
 
+
+//La généralité
 public class Spacecraft {
+
+    protected String name;
+    protected int life;
+    protected int maxLife;
+    protected int stamina;
+    protected int maxStamina;
+
+
+    public Spacecraft(String name){
+        this.name = name;
+        this.life = 100;
+        this.maxLife = 100;
+    }
+    public Spacecraft(){
+
+    }
+
+
+
 }
