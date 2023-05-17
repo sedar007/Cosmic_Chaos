f1a3d9416ffd431a9a927249b88ed9bbf1e92616
sedar <desiradjysedar@gmail.com>
2023-05-17 11:59:36 +0200

    stats

---
+++ core/src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+import spacecraft.Boss;
+import spacecraft.Monster_2;
+
+public class Test {
+    public static void main (String[] args) {
+        Boss monster = new Boss();
+        System.out.println(monster);
+    }
+}
