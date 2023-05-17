a68949ba3857c899ca8e3028107283b7e9a1e2c4
sedar <desiradjysedar@gmail.com>
2023-05-17 14:02:19 +0200

    stats

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import shoot_em_up.ShootEmUP;
+import test.Test;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
@@ -12,5 +13,8 @@ public class DesktopLauncher {
 		config.setTitle("Shoot_em_up");
 		config.setWindowedMode(1000, 1000);
 		new Lwjgl3Application(new ShootEmUP(), config);
+
+
+
 	}
 }
---
+++ desktop/src/com/mygdx/game/TestLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import test.Test;
+
+public class TestLauncher {
+    public static void main (String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        new Lwjgl3Application(new Test());
+
+
+
+    }
+}
+
+
