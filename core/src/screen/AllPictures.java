9deb5afc024fc9c7ade592f46d4a896689ccbc27
Sedar <desiradjysedar@gmail.com>
2023-05-26 15:06:44 +0200

    ammo

---
+++ core/src/screen/AllPictures.java
@@ -0,0 +1,16 @@
+package screen;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class AllPictures {
+
+    private Texture missilePicture;
+    private Texture Missiles;
+    private Texture Missiles;
+    private Texture Missiles;
+
+
+    public AllPictures(){
+
+    }
+}
